3106a9636785aac074224ad758a7e26172ff6cef
Marko Janjic <marko.m.janjic@gmail.com>
2022-10-12 01:10:39 +0200

    js code refactoring

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ let hobbit = new Book("Hobbit", "JRR Tolkein", 300, "Read");
 books.push(hobbit)
 getLibrary();
 
+modalBtn.addEventListener("click", () => {
+    modal.classList.toggle("show-modal");
+});
+
 myForm.addEventListener("submit", function (e) {
     e.preventDefault();
     readStatus.checked ?
@@ -32,43 +36,31 @@ myForm.addEventListener("submit", function (e) {
         readStatus.value
     );
     books.push(newBook);
-    console.log(books);
     getLibrary();
     modal.classList.remove("show-modal");
 });
 
-closeModal.addEventListener('click',()=>{
+closeModal.addEventListener('click', () => {
     modal.classList.remove("show-modal")
 })
+
 function displayBooks() {
     app.innerHTML = "";
     books.map(function (book) {
+        const elementArray = []
         const card = document.createElement("div");
-        const cardTitle = document.createElement("h2");
-        const cardAuthor = document.createElement("h2");
-        const cardPages = document.createElement("p")
-        const cardBtn = document.createElement("button");
-        const closeBtn = document.createElement("button");
-        const span = document.createElement("span");
-        const span2 = document.createElement("span");
-        const span3 = document.createElement("span");
+        generateElement('span', 'Title', elementArray)
+        generateElement('h2', book.title, elementArray)
+        generateElement('span', 'Author', elementArray)
+        generateElement('h2', book.author, elementArray)
+        generateElement('span', 'Pages', elementArray)
+        generateElement('p', book.pages, elementArray)
+        const cardBtn = generateElement('button', book.status, elementArray)
+        const closeBtn = generateElement('button', 'Remove', elementArray)
         app.appendChild(card);
-        card.appendChild(span);
-        card.appendChild(cardTitle);
-        card.appendChild(span2);
-        card.appendChild(cardAuthor);
-        card.appendChild(span3)
-        card.appendChild(cardPages)
-        card.appendChild(cardBtn);
-        card.appendChild(closeBtn);
-        span.textContent = "Title";
-        span2.textContent = "Author";
-        span3.textContent = 'Pages'
-        cardTitle.textContent = book.title;
-        cardAuthor.textContent = book.author;
-        cardPages.textContent = book.pages
-        cardBtn.textContent = book.status;
-        closeBtn.textContent = "Remove";
+        for (element of elementArray) {
+            card.appendChild(element)
+        }
         card.classList.add("card");
         closeBtn.classList.add("close-btn");
         cardBtn.classList.add("card-btn");
@@ -105,7 +97,6 @@ function changeReadStatus() {
 
 function removeBook() {
     const cards = document.querySelectorAll(".card");
-    console.log(cards);
     cards.forEach((card) => {
         const closeBtn = card.querySelector(".close-btn");
         const cardTitle = card.children[1];
@@ -126,11 +117,14 @@ function getLibrary() {
         removeBook();
     } else {
         app.innerHTML = "";
-        let title = document.createElement("h1");
-        title.textContent = "No Books added?";
+        let title = generateElement('h1', 'No books added', null)
         app.appendChild(title);
     }
 }
-modalBtn.addEventListener("click", () => {
-    modal.classList.toggle("show-modal");
-});
+
+function generateElement(el,content = '', arr = null) {
+    const element = document.createElement(el);
+    content ? element.textContent = content : false
+    arr ? arr.push(element) : false
+    return element
+}
